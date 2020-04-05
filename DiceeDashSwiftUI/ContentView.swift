8eb562ba5a8c02acdf42e529f9b2dbeb7590a634
Saul Rivera <saulriveraal@icloud.com>
2020-04-05 00:00:16 -0600

    Layout Design

--- DiceeDashSwiftUI/ContentView.swift
+++ DiceeDashSwiftUI/ContentView.swift
@@ -10,9 +10,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(n: 1)
+                    DiceView(n: 1)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 40))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }
+                .background(Color.red)
+            }
+        }
     }
 }
+
+struct DiceView: View {
+    let n: Int
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding(.all)
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
